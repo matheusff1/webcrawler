f179225cc480632f809a9d0f155903e7f14943f6
matheusff1 <ths2ffo@gmail.com>
2025-02-25 10:46:46 -0300

    att

--- scrapper/scrapper/spiders/RankingTheBrands.py
+++ scrapper/scrapper/spiders/RankingTheBrands.py
@@ -4,14 +4,15 @@ import scrapy
 class RankingthebrandsSpider(scrapy.Spider):
     name = "RankingTheBrands"
     allowed_domains = ["rankingthebrands.com"]
-    start_urls = ["https://www.rankingthebrands.com/The-Brands-and-their-Rankings.aspx"]
+    start_urls = ["https://www.rankingthebrands.com/The-Brands-and-their-Rankings.aspx?catFilter=0&nameFilter=A"]
 
     def parse(self, response):
-        for index in response.css('ctl00_mainContent_filterPanel'):
-            print(index)
-            for brand in response.css('ctl00_mainContent_brandPanel'):
-                print(brand)
-                yield{
-                    'index':index.css('::text').get(),
-                    'name':brand.css('::text').get(),
+        
+            for brand in response.css('#ctl00_mainContent_brandPanel .brandLine'):
+                brand_name = brand.css('a span.rankingName::text').get()
+
+            
+                yield {
+                    'name': brand_name,
+                    'letter': 'a',
                 }
